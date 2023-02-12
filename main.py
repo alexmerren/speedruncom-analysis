642868cdd0451b38181daa9f205848d2fd48a5be
alexmerren <alexandermerren@gmail.com>
2023-02-12 20:02:25 +0000

    *

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from collector import collector
 
 def main():
-    #collector.GameInformationCollector("all games").run()
-    collector.RelatedGamesCollector().run(game_start_index=8412, player_start_index=0)
-    #collector.CollatedRelatedGamesCollector("all games").run(game_start_index=0, player_start_index=0)
+    collector.RelatedGamesCollector().run(game_start_index=21_000, player_start_index=0)
 
 if __name__ == "__main__":
     main()
