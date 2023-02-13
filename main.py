5c13dc5e1dfed36c96c3f2eb7835779e93679e6d
Alex Merren <alexandermerren@gmail.com>
2023-02-13 12:25:45 +0000

    Got to 9901/32000

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from collector import collector
 
 def main():
-    collector.RelatedGamesCollector().run(game_start_index=8412, player_start_index=0)
+    collector.RelatedGamesCollector().run(game_start_index=9901, player_start_index=0)
 
 if __name__ == "__main__":
     main()
