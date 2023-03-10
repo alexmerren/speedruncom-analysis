e8d252f7682a7cb622a107792f9447318c8ee9c1
alexmerren <alexandermerren@gmail.com>
2023-03-10 18:23:39 +0000

    *

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 from collector import collector
 
 def main():
-    # collector.RelatedGamesInformationCollater("games network 10 percent", debug=1).run(percentage_limit=0.1)
-    # collector.RelatedGamesInformationCollater("games network full", debug=1).run(percentage_limit=1)
-
-    # Spot collection attempt for subway surfers, it still 5XX's.
-    # collector.GameInformationCollector("test", debug=1).run(start_index=16402)
+    collector.RelatedGamesInformationCollater("all games", debug=1).run(percentage_limit=1)
 
 if __name__ == "__main__":
     main()
