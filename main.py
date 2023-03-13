b0b00a1f63031880ff4aa862096a96c7177f976d
alexmerren <alexandermerren@gmail.com>
2023-03-13 17:50:07 +0000

    *

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from collector import collector
 
 def main():
     # The name field is blank as this API does not write to disk, it creates writeApis internally.
-    collector.CollatedWorldRecordHistoryCollector("", debug=1).run(game_start_index=28095)
+    collector.CollatedWorldRecordHistoryCollector("", debug=1).run(game_start_index=27575)
 
 if __name__ == "__main__":
     main()
